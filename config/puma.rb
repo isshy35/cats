73ca87ceaead48ec62838f0e0f4f4badedb93dcc
isshy <isshy@gmail.com>
2025-04-11 21:01:41 +0000

    initial

--- config/puma.rb
+++ config/puma.rb
@@ -4,7 +4,7 @@ threads_count = Integer(ENV.fetch('MAX_THREADS', 1))
 threads       threads_count, threads_count
 workers       Integer(ENV.fetch('WEB_CONCURRENCY', 1))
 
-port          ENV.fetch('PORT', 8000)
+port          ENV.fetch('PORT', 8001)
 environment   ENV.fetch('RACK_ENV', 'production')
 rackup        'config.ru'
 
---
+++ start_app.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd /home/ubuntu/cats
+bundle exec puma -C config/puma.rb
